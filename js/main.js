85fbd5fc6d342ef0c6af65b8702a0bab25b20e65
Albina <darkcrashma@mail.ru>
2019-02-19 17:38:10 +0800

    #add: Добавлены блоки с видео, о компании, статистика, преимущства

--- js/main.js
+++ js/main.js
@@ -4,8 +4,13 @@ $(document).ready( function() {
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // nextArrow: '<div class="article-slider__arrow article-slider__arrow--next"><svg><use xlink:href="img/sprites.svg#next"></use></svg></div>', // Кастомная стрелка "далее"
-        // prevArrow: '<div class="article-slider__arrow article-slider__arrow--prev"><svg><use xlink:href="img/sprites.svg#prev"></use></svg></div>'
+    });
+
+    $('.featured-work-slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
     });
 
 });
