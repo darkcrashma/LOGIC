84bf81572dc10ca52fca847e62b70c552dbb9681
Albina <darkcrashma@mail.ru>
2019-02-20 17:34:19 +0800

    #add: добавлен аккордеон

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ $(document).ready( function() {
     });
 
 });
+
+$('.why-list__item--active').find('.why-list__text').slideDown();
+
+function accordeon () {
+    var $title = $('.why-list__title');
+    var $cont = $('.why-list__text');
+    var $clos = $('.why-list__item');
+    var classActive = 'why-list__title--active';
+    $title.on('click', function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        if(!$this.hasClass(classActive)) {
+            $cont.slideUp();
+            $title.removeClass(classActive);
+            $clos.removeClass('why-list__item--active');
+        }
+        $this.closest($clos).toggleClass('why-list__item--active');
+        $this.next($cont).slideToggle();
+        $this.toggleClass(classActive);
+    });
+}
+accordeon();
